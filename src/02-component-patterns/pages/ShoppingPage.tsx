3df025e04af8e20a826986cb51be24c147f973eb
pblgllgs <pbl.gllgs@gmail.com>
2022-03-25 08:44:05 -0300

    control props implement

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -5,14 +5,13 @@ import {
     ProductsButtons,
 } from '../components';
 import '../styles/custom-styles.css';
-
-const product = {
-    id: '1',
-    title: 'Coffee Mug',
-    img: './coffee-mug.png',
-};
+import { useShoppingCart } from '../hooks/useShoppingCart';
+import { products } from '../../data/products';
 
 export const ShoppingPage = () => {
+    
+    const { shoppingCart , onProductCountChange} = useShoppingCart();
+
     return (
         <div>
             <h1>Shopping Page</h1>
@@ -24,39 +23,42 @@ export const ShoppingPage = () => {
                     flexWrap: 'wrap',
                 }}
             >
-                <ProductCard className="bg-dark text-white" product={product}>
-                    <ProductCard.Image className="custom-image" />
-                    <ProductCard.Title className="text-white text-bold" />
-                    <ProductCard.Buttons className="custom-buttons" />
-                </ProductCard>
-                <ProductCard className="bg-dark text-white" product={product}>
-                    <ProductImage className="custom-image" />
-                    <ProductTitle className="text-white text-bold" />
-                    <ProductsButtons className="custom-buttons" />
-                </ProductCard>
-                <ProductCard
-                    style={{
-                        backgroundColor: '#70D1F8',
-                    }}
-                    product={product}
-                >
-                    <ProductImage
+                {products.map((product) => (
+                    <ProductCard
+                        key={product.id}
+                        className="bg-dark text-white"
+                        product={product}
+                        onChange={onProductCountChange}
+                        value={shoppingCart[product.id]?.count || 0}
+                    >
+                        <ProductImage className="custom-image" />
+                        <ProductTitle className="text-white text-bold" />
+                        <ProductsButtons className="custom-buttons" />
+                    </ProductCard>
+                ))}
+            </div>
+            <div className="shopping-cart">
+                {Object.entries(shoppingCart).map(([key, product]) => (
+                    <ProductCard
+                        key={key}
+                        className="bg-dark text-white"
+                        product={product}
                         style={{
-                            boxShadow: '10px 10px 10px rgba(0,0,0,0.2)',
+                            width: '100px',
                         }}
-                    />
-                    <ProductTitle
-                        style={{
-                            fontWeight: 'bold',
-                        }}
-                    />
-                    <ProductsButtons
-                        style={{
-                            display: 'flex',
-                            justifyContent: 'end',
-                        }}
-                    />
-                </ProductCard>
+                        value={product.count}
+                        onChange={(event) => onProductCountChange(event)}
+                    >
+                        <ProductImage className="custom-image" />
+                        <ProductsButtons
+                            className="custom-buttons"
+                            style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                            }}
+                        />
+                    </ProductCard>
+                ))}
             </div>
         </div>
     );
---
+++ src/data/products.ts
@@ -0,0 +1,13 @@
+import { Product } from '../02-component-patterns/interfaces/interfaces';
+const product = {
+    id: '1',
+    title: 'Coffee Mug',
+    img: './coffee-mug.png',
+};
+const product2 = {
+    id: '2',
+    title: 'Coffee Mug 2',
+    img: './coffee-mug2.png',
+};
+
+export const products: Product[] = [product, product2];
